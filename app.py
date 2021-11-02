31dfc050f32fe526da0ecc9fd9f32d711abebb1a
itsmebabysmiley <54635089+itsmebabysmiley@users.noreply.github.com>
2021-11-02 21:07:58 +0700

    implementing search()

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 from elasticsearch import Elasticsearch
-from werkzeug.wrappers import response
-
-# es = Elasticsearch()
+import json
+es = Elasticsearch()
 
 app = Flask(__name__)
 
@@ -13,19 +11,37 @@ def hello_world():
     return render_template('homepage.html')
 
 
-@app.route("/search")
+@app.route('/search', methods=['GET'])
 def search():
+    keyword = request.args.get('keyword')
     body = {
         'size' : '10',
-        'query' : {
-            "match_all" : {}
+         'query': {
+            'multi_match': {
+                'query': keyword,
+                'fields': ['name','short_story','characters','genres']
+            }
         }
     }
-    # res = es.search(index="",body=body)
-    '''
-    continue...
-    '''
-    return render_template('search.html',)
+
+    res = es.search(index='magna_index', doc_type='', body=body)
+    result = []
+    for r in res['hits']['hits']:
+        
+        body ={
+            'name' :        r['_source']['name'],
+            'related name': r['_source']['related name'],
+            'characters':   r['_source']['characters'],
+            'genres' :     [c.capitalize() for c in r['_source']['genres']],
+            'author' :      r['_source']['author'],
+            'publisher' :   r['_source']['Publisher']
+        }
+        result.append(body)
+    
+    json_formatted_str = json.dumps(result, indent=2)
+    print(json_formatted_str)
+        
+    return render_template('search.html', res=result)
     
     
     
